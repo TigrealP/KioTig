5dc0ebc8648b72509004020e3b8d05b81fd46a4a
Ccarretero <carreteroestudios@gmail.com>
2026-03-04 23:21:08 -0500

    Actualización de comando accion, se agregaron imagenes.

--- data/moodActions.js
+++ data/moodActions.js
@@ -1,31 +1,40 @@
 module.exports = {
     mimoso: [
         {
-            label: 'Abrazar',
-            value: 'hug',
+            label: 'Acariciar',
+            value: 'pat',
             embed: {
                 color: '#f472b6',
-                title: '🤗 Abrazo mimoso',
-                description: '{author} abrazó con cariño a {target} 💞',
+                title: '🐯🐶 Una linda caricia de parte de {author}',
+                description: '{author} acarició con cariño a {target} 💞',
                 images: [
-                    'https://media.tenor.com/3wR9-6PBJ7UAAAAC/anime-hug.gif',
-                    'https://media.tenor.com/xI8X94Z_emgAAAAC/anime-hug-anime.gif'
+                    'https://preview.redd.it/petting-session-randitawu-v0-l1txwlujsmne1.jpeg?width=640&crop=smart&auto=webp&s=8bab6a8d2464fd135ae888bc44d50aa469615dda',
+                    'https://external-preview.redd.it/petting-overload-zempy3-v0-LODpqerJCK2TG0VsnrdsXoqC8WAhYsPF7RI-8hslxJc.png?width=1080&crop=smart&auto=webp&s=50222b5047952755f57cee6e8427cc928222a8a0',
+                    'https://scontent.feoh3-1.fna.fbcdn.net/v/t39.30808-6/577135181_842790888327563_634573314472143491_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=13d280&_nc_ohc=TY0gbkUM0REQ7kNvwF_ktTZ&_nc_oc=AdmSOJXo7lRCj2fCSr8pWGO7P6ejbcmreH-zPnYfm2AkpeEpPs4UwKJbV_ffyyFJFPu2kKF3ZOGH3vqu_TKbwgxo&_nc_zt=23&_nc_ht=scontent.feoh3-1.fna&_nc_gid=nLYQm37Pk6HaP1CyTBIsiQ&_nc_ss=8&oh=00_AfxyzMjnb-st4CUxGAWvu2LjZYLg5PvCb9VKaGgimMs-zA&oe=69AE9B44',
+                    'https://scontent.feoh3-1.fna.fbcdn.net/v/t39.30808-6/579159864_842783821661603_1458565516172547961_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=13d280&_nc_ohc=3xPISXHHYQ4Q7kNvwHNwWy8&_nc_oc=AdkvDqx1yzqhMwSSMYB_nVzX_Q2MzpB04tkNbf5q-2rlNvXVhCVmFjkYHhU8N0kzMfgf7dCYbvoRXfNmXQR-XkjC&_nc_zt=23&_nc_ht=scontent.feoh3-1.fna&_nc_gid=v5waPsKNb_KqVRnyaw7qmQ&_nc_ss=8&oh=00_Afx8lUKfpMCGZ3nu_i0nUvrklk2MH3aJS6uXTQp8CM6lrQ&oe=69AEBE2F',
+                    'https://pbs.twimg.com/media/EyJpFhnW8AYnmU6.jpg',
+                    'https://d.furaffinity.net/art/sabby/1683950100/1683950100.sabby_dfwq8x2-0c507e46-679f-445e-97a7-58150c16645d.png',
+                    'https://d.furaffinity.net/art/zawmg/1495272462/1495272462.zawmg_big_pawbs.png'
                 ],
                 footer: 'Los mimos nunca sobran 🐯🐶'
             }
         },
         {
-            label: 'Besito',
-            value: 'kiss',
+            label: 'Acurrucar',
+            value: 'acurrucar',
             embed: {
-                color: '#fb7185',
-                title: '💋 Besito activado',
-                description: '{author} le dio un besito a {target} ✨',
+                color: '#7e0404',
+                title: '🐶🐯 Acurrucados',
+                description: '{author} se acurrucó con {target} 💕',
                 images: [
-                    'https://media.tenor.com/jM1Q74hKQzEAAAAC/anime-kiss.gif',
-                    'https://media.tenor.com/kCZjTqCKiggAAAAC/anime-kiss.gif'
+                    'https://i.pinimg.com/736x/26/05/fb/2605fb43b462ce126a729e6cde5ab95c.jpg',
+                    'https://i.pinimg.com/736x/cb/19/57/cb1957fd32fe96e156fda58b4be236db.jpg',
+                    'https://i.pinimg.com/736x/af/10/9d/af109d280d7385b27e1cb143fc73d56c.jpg',
+                    'https://i.pinimg.com/736x/59/d5/72/59d572aac500f903744e93c79d187fbe.jpg',
+                    'https://i.pinimg.com/736x/3c/70/26/3c7026f3610d40d40967feb104e95cae.jpg',
+                    'https://i.pinimg.com/736x/9e/1c/9f/9e1c9f7911110165beaa5e5fea193f7b.jpg'
                 ],
-                footer: 'Nivel de ternura: máximo'
+                footer: 'Que bonito es acurrucarse con tu persona favorita -> 🐶'
             }
         }
     ],
@@ -34,56 +43,179 @@ module.exports = {
             label: 'Ponerle una correa',
             value: 'correa',
             embed: {
-                color: '#a855f7',
-                title: '🦮 Correa colocada',
-                description: '{author} le puso una correa a {target} con firmeza 😳',
-                footer: 'Dinámica activada'
+                color: '#7e0404',
+                title: '🦮 Correa colocada 🐶',
+                description: '{author} le puso una correa a {target} con firmeza 🐯🔥',
+                footer: 'Al parecer alguien es un cachorrito travieso, y se ve adorable! 🐾',
+                images: ['https://static1.e621.net/data/sample/8b/ce/8bce3559212bc0a34b0039b177623152.jpg',
+                    'https://static1.e621.net/data/sample/19/10/1910ecbf1487946f92bc69b5baf53c47.jpg',
+                    'https://static1.e621.net/data/sample/18/27/182714b71514e4c46cc72cc0237f0b72.jpg',
+                    'https://static1.e621.net/data/sample/e9/21/e921eff88e2fb3d6a2c22de7cda74026.jpg',
+                    'https://static1.e621.net/data/sample/4f/b2/4fb222724556475a3235f1eb158616e3.jpg',
+                    'https://static1.e621.net/data/sample/76/48/7648907bf3ee86d90d91560f4b1a05bd.jpg',
+                    'https://static1.e621.net/data/sample/74/2b/742bd20fad9bf26d91be5d64b0deff86.jpg'
+                ]
             }
         },
         {
             label: 'Lamer',
             value: 'lick',
             embed: {
-                color: '#c084fc',
-                title: '👅 Lamer',
-                description: '{author} decidió lamer a {target} 💜',
-                footer: 'Demasiada intensidad por aquí'
+                color: '#7e0404',
+                title: 'Al parecer {target} es un cachorrito delicioso 🐶',
+                description: '{author} tiene órdenes claras, con su boca y tu boquita, perrito 🐶🥂',
+                footer: 'al parecer alguien es un cachorrito muy sabroso 🐾',
+                images: ['https://static1.e621.net/data/sample/c8/57/c8570225aef36f8c47d69189d682cb9f.jpg',
+                    'https://static1.e621.net/data/sample/73/e9/73e95104d95a3a56bd47c5407bd060ce.jpg',
+                    'https://static1.e621.net/data/sample/5c/7f/5c7f74833a66c59537b39be793a3c12a.jpg',
+                    'https://static1.e621.net/data/sample/4b/d5/4bd5cf3c10713c5481ba93fb8cf4a44e.jpg',
+                    'https://static1.e621.net/data/sample/21/a9/21a9b11a148af91b71153720979e3228.jpg',
+                    'https://static1.e621.net/data/sample/53/cb/53cb2308661f2c1e0f847b2479904a6f.jpg'
+                ]
+            }
+        },
+        {
+            label: 'nalguear',
+            value: 'nalgada',
+            embed: {
+                color: '#f43f5e',
+                title: '🍑 Nalgada recibida',
+                description: '{author} le dio una nalgada a {target} 🍑',
+                footer: 'Al parecer {target} disfruta de estas atenciones 🐾',
+                images: ['https://static1.e621.net/data/sample/c4/8d/c48d1097882acf15fa3398ff837702ee.jpg',
+                    'https://static1.e621.net/data/sample/6a/0e/6a0e622c4a456b38f21ecbe6ee3e10fc.jpg',
+                    'https://static1.e621.net/data/c7/c1/c7c16d1d23488695f14584f0e0142c95.gif',
+                    'https://static1.e621.net/data/sample/e3/4d/e34d7f355579c68cb76a501825e922ca.jpg',
+                    'https://static1.e621.net/data/sample/25/5f/255faf7040749ffff94d3dee179fcf15.jpg',
+                    'https://static1.e621.net/data/sample/f2/8f/f28f792b149304d5b23072aaa6c1e239.jpg'
+                ]
             }
         }
     ],
     dominante: [
         {
-            label: 'Tomar de la cintura',
-            value: 'waist',
+            label: 'Arrodillarse',
+            value: 'arrodillar',
             embed: {
                 color: '#f97316',
-                title: '🔥 Presa de cintura',
-                description: '{author} tomó de la cintura a {target} y no lo soltó 😏',
+                title: '{author} se arrodilló ante {target} 🐯',
+                description: '{author} se arrodilló ante {target} mostrando su sumisión 🐶',
+                images: ['https://static1.e621.net/data/sample/2a/be/2abe251e497591523821bec9c28d00b8.jpg',
+                    'https://static1.e621.net/data/sample/8a/14/8a14cf4cecae0c5c7295ba7903cf5af4.jpg',
+                    'https://static1.e621.net/data/sample/fe/99/fe992a7a27c48b25457b662b572a888d.jpg',
+                    'https://static1.e621.net/data/sample/0e/cd/0ecd0155fdcccc0bb7898d4cdb62f01f.jpg',
+                    'https://static1.e621.net/data/sample/ab/f3/abf32546208fed48be7d68a87de17ada.jpg',
+                    'https://static1.e621.net/data/sample/2f/40/2f4082356924a40f8aefe197caa1a06a.jpg',
+                    'https://static1.e621.net/data/sample/e9/08/e908173c796686e37de01c7b2de8f667.jpg',
+                    'https://static1.e621.net/data/sample/0b/df/0bdfe337ebb394965373119b5a2d407c.jpg',
+                    'https://static1.e621.net/data/sample/b7/8b/b78b136477563dbd6fc1b05a80c2fda3.jpg'
+                ],
                 footer: 'Dominancia en curso'
+            }
+        },
+        {
+            label: 'Prepararse',
+            value: 'preparar',
+            embed: {
+                color: '#f97316',
+                title: '{author} está listo para ser usado por {target} 🐯',
+                description: 'Al parecer alguien ya está preparado para su amo 🐶🐯',
+                images: ['https://static1.e621.net/data/sample/8d/c9/8dc9fcaa37935c2cadbe89d4597ce9ec.jpg',
+                    'https://static1.e621.net/data/d6/c3/d6c3b4b3ec17880c5d6ff0e45857544d.jpg',
+                    'https://static1.e621.net/data/sample/24/02/2402704047103134610fe7d7fb61caa4.jpg',
+                    'https://static1.e621.net/data/sample/24/5f/245f5c6e57b90068d7db29b9b0b4abec.jpg',
+                    'https://static1.e621.net/data/sample/72/d1/72d165b55b430934b8a57ece15d66979.jpg',
+                    'https://static1.e621.net/data/sample/88/15/88151705e3dcf5b86dedc4d2359a42b9.jpg',
+                    'https://static1.e621.net/data/sample/6f/1b/6f1bea28ede37fc8609b0090d67860b5.jpg',
+                    'https://static1.e621.net/data/sample/1a/93/1a93eeeaaf41ea78bb9981f575eef73d.jpg',
+                    'https://static1.e621.net/data/sample/15/a5/15a5f9ba45a0ea75f17a8002d963908b.jpg',
+                    'https://static1.e621.net/data/sample/08/5a/085ae65bee52b469232240b7fccbcb33.jpg'
+                ],
+                footer: 'Preparación en curso'
+            }
+        },
+        {
+            label: 'Provocación',
+            value: 'provocar',
+            embed: {
+                color: '#f97316',
+                title: '{author} provoca a {target} con intención maliciosa 🐯',
+                description: '{author} provoca a {target}, al parecer alguien quiere ser tomado 🐶',
+                images: ['https://static1.e621.net/data/sample/2a/be/2abe251e497591523821bec9c28d00b8.jpg',
+                    'https://static1.e621.net/data/sample/e7/0a/e70a40b9c1ddd1377404bdc75558ff1b.jpg',
+                    'https://static1.e621.net/data/sample/62/17/621745c7df3ae20d99cfc859fbf4f191.jpg',
+                    'https://static1.e621.net/data/sample/82/97/8297e786f51a31cbc407a5e0ee416572.jpg',
+                    'https://static1.e621.net/data/sample/2c/cc/2ccc6d6cc2a06e962a90367aa6445398.jpg',
+                    'https://static1.e621.net/data/sample/1c/f8/1cf84641e2a34fbd8488459cdef97af9.jpg',
+                    'https://static1.e621.net/data/sample/ad/43/ad4397fab177dfdd45b4d170890e3618.jpg',
+                    'https://static1.e621.net/data/sample/0d/24/0d24c20e649e56f0f60f53c0a7c18400.jpg',
+                    'https://static1.e621.net/data/sample/4d/37/4d37d58cb6a4902f1c2b1cea67a417bb.jpg'
+                ],
+                footer: 'Al parecer está funcionando 🐯🔥'
             }
         }
     ],
     needy: [
         {
-            label: 'Mimos',
-            value: 'mimos',
+            label: 'Abrazo',
+            value: 'abrazo',
             embed: {
                 color: '#60a5fa',
-                title: '🫶 Atención requerida',
-                description: '{author} está dándole mimos intensivos a {target} 🥺',
+                title: 'Tu cachorrito / dueño te ha dado un abrazo 🐶🐯',
+                description: '{author} está dándole mimos intensivos a {target} 🐯🐶',
+                images: ['https://i.pinimg.com/736x/0e/f0/82/0ef082c09737eeb93e3557ff8a559c60.jpg',
+                    'https://i.pinimg.com/736x/fe/0d/59/fe0d59c2d417be8721aa9bb16cf98e93.jpg',
+                    'https://i.pinimg.com/736x/86/8c/04/868c04701c4bca105fb8d463ec2257cc.jpg',
+                    'https://i.pinimg.com/736x/61/43/a7/6143a79a6a38f550236bac5cbfe97ef6.jpg',
+                    'https://i.pinimg.com/736x/19/00/1a/19001abf8ba15341158b8a25735a1176.jpg',
+                    'https://i.pinimg.com/736x/43/70/3c/43703cd2be388729943f9f0370284449.jpg',
+                    'https://i.pinimg.com/736x/56/0e/c8/560ec8e8a5c34fa6affc2538b172bc72.jpg',
+                    'https://i.pinimg.com/736x/b8/9b/dd/b89bddb1d36550483caf9a10641dba62.jpg'
+                ],
+                footer: 'No ignores estos ojitos'
+            }
+        },
+        {
+            label: 'Besitos',
+            value: 'besitos',
+            embed: {
+                color: '#60a5fa',
+                title: '{author} ha dado besitos a {target} 🐶🐯',
+                description: '{author} está dándole besitos a {target} 🐯🐶',
+                images: ['https://i.pinimg.com/736x/6e/b7/b3/6eb7b375303f9b6d57db5f6169d95658.jpg',
+                    'https://i.pinimg.com/736x/c5/34/49/c53449f7869c1f7fd4afb2c6b5f4d922.jpg',
+                    'https://i.pinimg.com/736x/92/f3/9e/92f39e728273360c86d97deda306d6cc.jpg',
+                    'https://i.pinimg.com/736x/b3/d3/34/b3d334603270a856c65df940c3ba5cc3.jpg',
+                    'https://i.pinimg.com/736x/9d/9d/3b/9d9d3bbe91ebc45bad970b38057ed297.jpg',
+                    'https://scontent.feoh3-1.fna.fbcdn.net/v/t1.6435-9/133504492_3117936701641786_6944475894296073849_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=13d280&_nc_ohc=E8WrljHdnPsQ7kNvwH4mbvw&_nc_oc=AdkYrvXfsSaF0j-js7Gil3txirRDmW4VVByPZnSulTpoLvsrS9Rn9gNVRGNju-eYEvgJNU472jA-pyioioRxtkDF&_nc_zt=23&_nc_ht=scontent.feoh3-1.fna&_nc_gid=j9k52QGFzJLLFOSPcKLUEg&_nc_ss=8&oh=00_AfxLbE3I9Ya67037U00nMPAjEQme6DPXsvuOm7c_e2ABgw&oe=69D06634',
+                    'https://d.furaffinity.net/art/poof-poof/1619630681/1619630681.poof-poof_xondarapril_s.png'
+                ],
                 footer: 'No ignores estos ojitos'
             }
         }
     ],
     celoso: [
         {
-            label: 'Desafiar',
-            value: 'challenge',
+            label: 'Poema',
+            value: 'poema',
             embed: {
-                color: '#ef4444',
-                title: '⚔️ Desafío por celos',
-                description: '{author} desafió a {target} porque hoy anda celoso 😤',
-                footer: 'Celos detectados'
+                color: '#580101',
+                title: '{author} ha escrito un poema para {target} 🐯',
+                description: 'Entre tantos días que pasan, entre tantas horas que se escapan, entre tantas cosas que cambian, hay algo en mí que permanece cuando pienso en ti. Eres ese pensamiento suave que aparece sin avisar, esa calma que encuentro incluso cuando estás lejos. A veces me sorprende lo fácil que es quererte, como si mi corazón ya supiera tu nombre desde antes. Y si alguna vez dudas, solo recuerda esto: de todas las historias que podría vivir, la que más quiero… es la que escribo contigo. ❤️',
+                footer: 'Alguien está sintiendo celos, pero de una forma muy tierna 🐶🐯🥂'
+            }
+        },
+        {
+            label: 'Atencion',
+            value: 'atencion',
+            embed: {
+                color: '#580101',
+                title: '{author} se ha comprometido a ser más atento con {target} 🐯🐶',
+                description: '{author} se ha dado cuenta de que no ha estado tan atento como debería con {target}, y se compromete a mejorar y darle toda la atención que merece 🐶🐯',
+                images: ['https://i.pinimg.com/736x/03/11/50/031150adbc219280fd4d46b3b0521017.jpg',
+                    'https://i.pinimg.com/736x/90/7b/fe/907bfe980d2d825e3bcd7b3ed046dbcc.jpg',
+                    'https://i.pinimg.com/736x/b8/e5/58/b8e558ba42bfe7f416c692e5dbd82159.jpg',],
+                footer: 'Alguien está sintiendo celos, pero de una forma muy tierna 🐶🐯🥂'
             }
         }
     ]
